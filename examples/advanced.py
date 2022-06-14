329f91ff44a33396057e181cddf07663c3d16086
Megan Hu <meganhu@Megans-MacBook-Pro.local>
2022-06-14 13:58:57 -0500

    First commit

---
+++ examples/advanced.py
@@ -0,0 +1,32 @@
+import latextable_lite
+
+"""
+Example showcasing all optional arguments:
+    num_headers, multi_column_size, caption, caption_above, and c_line.
+"""
+if __name__ == "__main__":
+    num_headers = 2
+    rows = [["Item", "Guitar", "Piano", "Flute", "Ukelele", "Mic"],
+            ["Setup", "A", "B", "A", "B", "A", "B", "A", "B", "A", "B"],
+            ["Uniform w/ replacement", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0"],
+            ["", "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}"],
+            ["Uniform w/o replacement", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0"],
+            ["", "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}"],
+            ["S-NeRF", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0"],
+            ["", "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}"],
+            ["RFE", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0"],
+            ["", "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}"],
+            ["RFE+", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0"],
+            ["", "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}",  "^{\pm 0.5}"]]
+    multi_column_size = [1,2,2,2,2,2]
+    caption = "advanced latexlite example"
+    caption_above = True,
+    c_line = True
+    
+    print(latextable_lite.draw_latex(rows, 
+                                    num_headers=num_headers,
+                                    multi_column_size=multi_column_size,
+                                    caption=caption,
+                                    caption_above=caption_above,
+                                    c_line=c_line))
+
